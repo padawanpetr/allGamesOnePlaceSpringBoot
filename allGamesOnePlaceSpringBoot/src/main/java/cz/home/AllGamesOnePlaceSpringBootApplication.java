24001a878688e14057cfacbef6e8356ee86580c8
Petr Slezák <p.slezak@o2.cz>
2018-01-11 11:57:58 +0100

    Initial commit

---
+++ allGamesOnePlaceSpringBoot/src/main/java/cz/home/AllGamesOnePlaceSpringBootApplication.java
@@ -0,0 +1,12 @@
+package cz.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllGamesOnePlaceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllGamesOnePlaceSpringBootApplication.class, args);
+	}
+}
---
+++ allGamesOnePlaceSpringBoot/src/test/java/cz/home/AllGamesOnePlaceSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package cz.home;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AllGamesOnePlaceSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
